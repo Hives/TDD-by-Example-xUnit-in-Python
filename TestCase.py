aa350de8186ec4cf33d26cc6fe7e2e4d9a6d8077
Paul Martin <paul.y.martin@johnlewis.co.uk>
2020-08-08 21:27:18 +0100

    test run method returns a TestResult object

--- TestCase.py
+++ TestCase.py
@@ -1,3 +1,6 @@
+from TestResult import TestResult
+
+
 class TestCase:
     def __init__(self, name):
         self.name = name
@@ -9,7 +12,10 @@ class TestCase:
         pass
 
     def run(self):
+        result = TestResult()
+        result.test_started()
         self.set_up()
         method = getattr(self, self.name)
         method()
         self.tear_down()
+        return result
--- test.py
+++ test.py
@@ -10,5 +10,11 @@ class TestCaseTest(TestCase):
         test.run()
         assert(test.log == "set_up test_method tear_down ")
 
+    def test_result(self):
+        test = WasRun("test_method")
+        result = test.run()
+        assert(result.summary() == "1 run, 0 failed")
+
 
 TestCaseTest("test_template_method").run()
+TestCaseTest("test_result").run()
